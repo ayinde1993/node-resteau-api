32852d9b804dac62cd3e4cbe7eed61e9f7998dc3
azolnonix <azizeolougbokiki93@gmail.com>
2025-02-18 00:57:04 +0100

    add find and update user endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,57 @@
+const userModel = require("../models/userModel");
 
 const getUserController =  async (req, res) => {
-    res.status(200).json("it working");
+   try {
+    const user = await userModel.findById({_id:req.body.userId});
+    if(!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found',
+        });
+    }
+    //hide password from response
+    user.password = undefined;
+    res.status(200).json({
+        success: true,
+        message: 'User get successfully',
+        user
+    });
+   } catch (error) {
+    res.status(500).json({
+        success: false,
+        message: 'Internal server error',
+        error
+    });
+   }
+};
+
+const updateUserController = async (req, res) => {
+    try {
+        const user = await userModel.findByIdAndUpdate({_id:req.body.userId}, req.body, {new:true});
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found',
+            });
+        }
+        //hide password from response
+        user.password = undefined;
+        
+        res.status(200).json({
+            success: true,
+            message: 'User updated successfully',
+            user
+        }); 
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error',
+            error
+        });
+    }
 };
 
 module.exports = { 
-    getUserController 
-}; 
+    getUserController,
+    updateUserController
+};     
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getUserController } = require("../controllers/userController"); 
+const { getUserController, updateUserController } = require("../controllers/userController"); 
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 
 
 router.get('/getuser', authMiddleware, getUserController); // get user data
+
+// update user data
+router.put('/updateuser', authMiddleware, updateUserController);    // update user data 
 
 
 module.exports = router;
